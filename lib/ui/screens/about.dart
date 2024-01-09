c5bebfe933cdeeb41a294bd572eda2da11c8eb92
Mr.Meeseeks <156107272+mr-Meeeeseeks@users.noreply.github.com>
2024-01-09 21:10:20 +0330

    Redesign about us

--- lib/ui/screens/about.dart
+++ lib/ui/screens/about.dart
@@ -1,15 +1,19 @@
+import 'dart:io';
+
 import 'package:defacto/ui/screens/configuration.dart';
 import 'package:defacto/ui/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:url_launcher/url_launcher_string.dart';
-
-// TODO: need to make more beautiful
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const gapBetweenCards = Gap(8);//this is the gap between cards
+    const gapBetweenHeaderAndElements = Gap(8);//this is the gap between header and elements in the card for example between title and description
+    const gapBetweenElementsInTheCard = Gap(0);//this is the gap between elements in the card
     return PopScope(
       canPop: false,
       onPopInvoked: (didPop) {
@@ -22,182 +26,217 @@ class AboutScreen extends StatelessWidget {
       },
       child: Scaffold(
         appBar: AppBar(
-          iconTheme: IconThemeData(color: Colors.white),
+          iconTheme: const IconThemeData(color: Colors.white),
           backgroundColor: Theme.of(context).colorScheme.primary,
           title: const Text("About", style: TextStyle(color: Colors.white)),
         ),
         drawer: const MainDrawer(),
         backgroundColor: Theme.of(context).colorScheme.background,
-        body: Material(
-          color: Theme.of(context).colorScheme.background,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+        //using listview to avoid overflow in small screen
+        body: ListView(
+          padding: const EdgeInsets.all(16.0),//padding for all sides in the listview
+          children: [
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Defacto Proxy App",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    gapBetweenHeaderAndElements,
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        "Bypassing the Great Firewall (GFW)",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            gapBetweenCards,
+            // Versions Card
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const MetaWithIcon(
+                      icon: Icons.update,
+                      title: "Version",
+                      description: "1.0.0",
+                    ),
+                    gapBetweenElementsInTheCard,
+                    const MetaWithIcon(
+                      icon: Icons.layers,
+                      title: "Version (sing-box)",
+                      description: "sing-box-1.0.0\nsing-box-1.0.1\nsing-box-2.0.0",
+                    ),
+                    gapBetweenElementsInTheCard,
+                    InkWell(
+                      onTap: (){
+                        //todo: redirect to the donate page
+                        debugPrint("I am tapped :)");
+                      },
+                      child: const MetaWithIcon(
+                        icon: Icons.wallet_giftcard_outlined,
+                        title: "Donate",
+                        description: "Well, Every penny counts!",
+                      ),
+                    ),
+                    gapBetweenElementsInTheCard,
+                    if(Platform.isAndroid)
+                      //add button to ignore battery optimization
+                      InkWell(
+                        onTap: (){
+                          //todo: ignore this feature requires install new package
+                          debugPrint("I am tapped :)");
+                        },
+                        child: const MetaWithIcon(
+                          icon: Icons.battery_1_bar_sharp,
+                          title: "Ignore Battery Optimization",
+                          description: "This will help to keep the app running in background",
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+            gapBetweenCards,
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Project",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    gapBetweenHeaderAndElements,
+                    InkWell(
+                      onTap: (){
+                        //open github project page
+                        launchUrlString("https://github.com/bepass-org");
+                      },
+                      child: const MetaWithIcon(
+                        icon: Icons.code,
+                        title: "Source code"
+                      ),
+                    ),
+                    gapBetweenElementsInTheCard,
+                    InkWell(
+                      onTap: (){
+                        //todo: add telegram group link
+                        debugPrint("I am tapped :)");
+                      },
+                      child: const MetaWithIcon(
+                          icon: Icons.telegram,
+                          title: "Telegram update channel"
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Links Card
+            gapBetweenCards,
+            // Licenses Card
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "License",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    gapBetweenHeaderAndElements,
+                    InkWell(
+                      onTap: (){
+                        //open github project page
+                        launchUrlString("https://github.com/bepass-org");
+                      },
+                      child: const MetaWithIcon(
+                          icon: Icons.copyright,
+                          title: "License details"
+                      ),
+                    ),
+                    gapBetweenHeaderAndElements,
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class MetaWithIcon extends StatelessWidget {
+  const MetaWithIcon({super.key, required this.icon, required this.title, this.description});
+  final IconData icon;
+  final String title;
+  final String? description;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),//this padding help clickable item easier to touch
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            icon,
+            size: 24,
+          ),
+          const Gap(16),
+          Expanded(//expanded to fill the remaining space
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Defacto Proxy App Card
-                Card(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Defacto Proxy App",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          "Bypassing the Great Firewall (GFW)",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black87,
-                          ),
-                        ),
-                        Divider(height: 20),
-                      ],
-                    ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
                 ),
-                const SizedBox(height: 20),
-
-                // Versions Card
-                Card(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Versions",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        ...List.generate(
-                          2,
-                          (index) => Row(
-                            children: [
-                              Icon(
-                                index == 0
-                                    ? Icons.android
-                                    : Icons.system_security_update,
-                                color: Theme.of(context).iconTheme.color,
-                              ),
-                              const SizedBox(width: 8),
-                              Expanded(
-                                child: Text(
-                                  index == 0
-                                      ? "Current Version: 1.0.0"
-                                      : "ABI Version: 2.0.0",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headlineMedium,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Divider(height: 20),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                // Links Card
-                Card(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton.icon(
-                            onPressed: () {
-                              launchUrlString("https://github.com/bepass-org");
-                            },
-                            icon: const Icon(Icons.code),
-                            label: const Text("View Source Code"),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                // Licenses Card
-                Card(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Row(
-                          children: [
-                            Text(
-                              "Licenses",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            Icon(
-                              Icons.book_rounded,
-                              size: 24,
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        ...List.generate(
-                          2,
-                          (index) => Row(
-                            children: [
-                              const SizedBox(width: 8),
-                              Expanded(
-                                child: Text(
-                                  index == 0 ? "MIT License" : "GNU License",
-                                  style:
-                                      Theme.of(context).textTheme.headlineSmall,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                if(description != null)
+                  Text(
+                  description!,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.black87,
                   ),
                 ),
               ],
             ),
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
